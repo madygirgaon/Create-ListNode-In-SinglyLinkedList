9f4905e5dce79868de8a4452447fee13ffbcbc8d
Mhallappa P. Girgaon <86066547+madygirgaon@users.noreply.github.com>
2021-09-01 15:51:07 +0530

    Add files via upload

---
+++ SinglyLinkedList.java
@@ -0,0 +1,20 @@
+package com.codewithmady.main;
+
+public class SinglyLinkedList {
+	
+	private ListNode head;
+	
+	public static class ListNode{
+		private ListNode next;
+		private int data;
+		
+		
+	
+		public ListNode(int data) {
+			this.data=data;
+			this.next=null;
+		}
+	}
+	
+
+}
